09126b3f29e04457b03d7156664dc44a67d950da
shatha-aj <sshatha.ss@hotmail.com>
2025-05-03 17:13:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -126,6 +126,6 @@ if submitted:
     severity = label_encoder.inverse_transform(prediction)
 
     st.success(f"🚨 Predicted Accident Severity: {severity[0]}")
-    probs = model.predict_proba(input_df)
+    probs = xgb.predict_proba(input_df)
 st.write("🔍 Prediction probabilities (per class):", probs)
 
